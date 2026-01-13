07c64f30e04329eb13df5cbd4b5cd99f0e317e1a
dilmurod <dilmurodaka@inbox.ru>
2026-01-13 22:15:24 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="d-flex flex-column p-3 bg-light" style="width: 256px;">
+                Kadirov Inc
+                <hr>
+                <div class="d-flex dropdown">
+                    <div>
+                        <img class="rounded-circle me-2" src="@/assets/ameliya.png" alt="" width="46" height="46">
+                    </div>
+                    <div class="">
+                        <span style="font-size: 14px;">Ameliya</span>
+                        <br />
+                        <span style="font-size: 11px;">ameliya.cer@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
