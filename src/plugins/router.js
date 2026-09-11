2e2eaead7a3cd2391d1c0580970f687818f2744f
fedora <dilmurodaka@inbox.ru>
2026-09-11 19:55:26 +0500

    get clients by-company continued

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -43,14 +43,6 @@ const routes = [
         beforeEnter: ifAuthorized
     },
     {
-        path: '/client-page/:id',
-        component: () => import('@/pages/ClientPage.vue'),
-        meta: {
-            layout: defineAsyncComponent(() => import('@/layouts/DefaultLayout.vue'))
-        },
-        beforeEnter: ifAuthorized
-    },
-    {
         path: '/settings',
         component: () => import('@/pages/SettinsPage.vue'),
         meta: {
